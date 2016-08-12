ad3df55f79a2587691e8aff357fcb87772415785
ugur.unlu <vally10@live.com>
2016-08-12 18:46:18 -0400

    Add project files.

---
+++ HtmlExtensions/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HtmlExtensions.MvcApplication" Language="C#" %>
---
+++ HtmlExtensions/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HtmlExtensions.Startup))]
+namespace HtmlExtensions
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
